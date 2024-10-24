e1563366171044e77807d5873857655a9b46dc01
Huỳnh Hữu Nghĩa <nghiahh.work@gmail.com>
2024-10-24 21:04:01 +0700

    Cart, detail

--- src/sections/cart/view/cart-view.tsx
+++ src/sections/cart/view/cart-view.tsx
@@ -1,14 +1,21 @@
-import { Box, Stack, useTheme, Container, Typography } from '@mui/material';
+import { Box, Link, Stack, useTheme, Container, Typography } from '@mui/material';
+
+import { paths } from 'src/routes/paths';
+import { RouterLink } from 'src/routes/components';
 
 import { pxToRem } from 'src/theme/styles';
 
+import { SvgColor } from 'src/components/svg-color';
 import { CustomBreadcrumbs } from 'src/components/custom-breadcrumbs';
 
 import CartList from '../cart-list';
 import CartInfo from '../cart-info';
+import { useCartContext } from '../context';
 
 export default function CartView() {
   const theme = useTheme();
+  const { products } = useCartContext();
+
   return (
     <Container
       maxWidth={false}
@@ -55,40 +62,91 @@ export default function CartView() {
       >
         YOUR CART
       </Typography>
-
-      <Stack direction={{ xs: 'column', md: 'row' }}>
-        <Box
+      {products.length === 0 ? (
+        <Stack
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          spacing={2}
           sx={{
-            pr: {
-              xs: 0,
-              md: pxToRem(49),
-            },
-            flex: 1,
+            paddingTop: '50px',
+            textAlign: 'center',
           }}
         >
-          <CartList />
-        </Box>
-        <Box
-          sx={{
-            mt: {
-              xs: pxToRem(82),
-              md: 0,
-            },
-            borderLeft: { xs: 0, md: '1px solid #000000' },
-            pl: { xs: 0, md: pxToRem(22), lg: pxToRem(44) },
-            pr: {
-              xs: 0,
-              md: 0,
-            },
-            width: { lg: 1 },
-            maxWidth: {
-              lg: pxToRem(444.8),
-            },
-          }}
-        >
-          <CartInfo />
-        </Box>
-      </Stack>
+          <Typography
+            variant="h6"
+            sx={{
+              fontWeight: 600,
+              width: '100%',
+            }}
+          >
+            Giỏ hàng của bạn đang trống
+          </Typography>
+          <Link
+            component={RouterLink}
+            href={paths.main.shop.root}
+            sx={{
+              color: 'black',
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <SvgColor
+              src="/assets/icons/arrow-left-bold.svg"
+              sx={{
+                height: pxToRem(9.09),
+                width: pxToRem(7),
+                mr: pxToRem(6.49),
+              }}
+            />
+            <Box
+              sx={{
+                fontSize: pxToRem(14),
+                lineHeight: '1.51222',
+                fontWeight: 600,
+              }}
+            >
+              Continue Shopping
+            </Box>
+          </Link>
+        </Stack>
+      ) : (
+        <Stack direction={{ xs: 'column', md: 'row' }}>
+          <Box
+            sx={{
+              pr: {
+                xs: 0,
+                md: pxToRem(49),
+              },
+              flex: 1,
+            }}
+          >
+            <CartList />
+          </Box>
+          <Box
+            sx={{
+              mt: {
+                xs: pxToRem(82),
+                md: 0,
+              },
+              borderLeft: { xs: 0, md: '1px solid #000000' },
+              pl: { xs: 0, md: pxToRem(22), lg: pxToRem(44) },
+              pr: {
+                xs: 0,
+                md: 0,
+              },
+              width: { lg: 1 },
+              maxWidth: {
+                lg: pxToRem(444.8),
+              },
+            }}
+          >
+            <CartInfo />
+          </Box>
+        </Stack>
+      )}
     </Container>
   );
 }
